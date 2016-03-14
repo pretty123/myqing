74ba186abbf1a85ec18ed87961342856883d3253
pretty123 <820168613@qq.com>
2016-03-14 12:09:05 +0800

    图文回复

--- addons/tuwen123/processor.php
+++ addons/tuwen123/processor.php
@@ -11,5 +11,8 @@ class Tuwen123ModuleProcessor extends WeModuleProcessor {
 	public function respond() {
 		$content = $this->message['content'];
 		//这里定义此模块进行消息处理时的具体过程, 请查看微擎文档来编写你的代码
+		$array[0]['title']="pretty";
+		$array[1]['title']="pretty123";
+		return $this->respNews($array);
 	}
 }
