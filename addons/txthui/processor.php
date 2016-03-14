3063f157e1e81f3bef56503bbfe9de5b5783b9b9
pretty123 <820168613@qq.com>
2016-03-14 11:25:11 +0800

    文字回复

--- addons/txthui/processor.php
+++ addons/txthui/processor.php
@@ -11,5 +11,6 @@ class TxthuiModuleProcessor extends WeModuleProcessor {
 	public function respond() {
 		$content = $this->message['content'];
 		//这里定义此模块进行消息处理时的具体过程, 请查看微擎文档来编写你的代码
+		return $this->respText($content);
 	}
 }
