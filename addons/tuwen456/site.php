9c7df960c0ee9cebee1bdba7b39b96f862e88d20
root <root@iZ947bhtlrcZ.(none)>
2016-03-14 14:26:36 +0800

    c

---
+++ addons/tuwen456/site.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 自定义图文-pretty模块微站定义
+ *
+ * @author pretty
+ * @url http://bbs.we7.cc/
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Tuwen456ModuleSite extends WeModuleSite {
+
+	public function doMobileIndex() {
+		//这个操作被定义用来呈现 功能封面
+	}
+
+}
