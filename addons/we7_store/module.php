493cb5fccf340792bd071f315efe30841de5be80
pretty123 <820168613@qq.com>
2016-03-15 11:26:03 +0800

    测试2

--- addons/we7_store/module.php
+++ addons/we7_store/module.php
@@ -6,35 +6,41 @@
  * @url http://bbs.we7.cc/
  */
 defined('IN_IA') or exit('Access Denied');
-
+ 
 class We7_storeModule extends WeModule {
-	public function fieldsFormDisplay($rid = 0) {
-		//要嵌入规则编辑页的自定义内容，这里 $rid 为对应的规则编号，新增时为 0
-	}
-
-	public function fieldsFormValidate($rid = 0) {
-		//规则编辑保存时，要进行的数据验证，返回空串表示验证无误，返回其他字符串将呈现为错误提示。这里 $rid 为对应的规则编号，新增时为 0
-		return '';
-	}
-
-	public function fieldsFormSubmit($rid) {
-		//规则验证无误保存入库时执行，这里应该进行自定义字段的保存。这里 $rid 为对应的规则编号
-	}
-
-	public function ruleDeleted($rid) {
-		//删除规则时调用，这里 $rid 为对应的规则编号
-	}
-
+ 
 	public function settingsDisplay($settings) {
+ 
+		// 声明为全局才可以访问到.
 		global $_W, $_GPC;
-		//点击模块设置时将调用此方法呈现模块设置页面，$settings 为模块设置参数, 结构为数组。这个参数系统针对不同公众账号独立保存。
-		//在此呈现页面中自行处理post请求并保存设置参数（通过使用$this->saveSettings()来实现）
+ 
+		// 验证表单来源安全
 		if(checksubmit()) {
+ 
+			// $_GPC 可以用来获取 Cookies,表单中以及地址栏参数
+			$dat = $_GPC['dat'];
+ 
+			// 验证表单, 通过 message() 方法提示用户操作错误信息
+			empty($dat['name']) && message('请填写便利店名称');
+			empty($dat['logo']) && message('请填写便利店 LOGO');
+			empty($dat['linkman']) && message('请填写便利店联系人');
+			empty($dat['phone']) && message('请填写便利店联系电话');
+			empty($dat['address']) && message('请填写便利店地址');
+			empty($dat['description']) && message('请填写便利店介绍');
+ 
 			//字段验证, 并获得正确的数据$dat
-			$this->saveSettings($dat);
+			if (!$this->saveSettings($dat)) {
+				message('保存信息失败','','error');   // 保存失败
+			} else {
+				message('保存信息成功','','success'); // 保存成功
+			}
 		}
-		//这里来展示设置项表单
+ 
+		// 模板中需要用到 "tpl" 表单控件函数的话, 记得一定要调用此方法.
+		load()->func('tpl');
+ 
+		// 调用模板, 展示参数设置表单
 		include $this->template('setting');
 	}
-
+ 
 }
