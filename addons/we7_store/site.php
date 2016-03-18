d2ed74d9a61ca8057fe6b195436f62dc3f37f8b4
pretty123 <820168613@qq.com>
2016-03-18 15:19:50 +0800

    goods

--- addons/we7_store/site.php
+++ addons/we7_store/site.php
@@ -10,12 +10,131 @@ defined('IN_IA') or exit('Access Denied');
 class We7_storeModuleSite extends WeModuleSite {
 	// category 表名
 	private $tb_category = 'we7_store_category';
+	// 商品表
+	private $tb_goods = 'we7_store_goods';
+	
+	/**
+	 * 获取商品状态的文字描述
+	 * @param int $status
+	 * @return string
+	 */
+	private function getStatus($status){
+		$status = intval($status);
+		if ($status == 1) {
+			return '下架';
+		} elseif ($status == 2) {
+			return '上架';
+		} else {
+			return '未知';
+		}
+	}
 	public function doMobileStore() {
 		//这个操作被定义用来呈现 功能封面
 	}
-	public function doWebGoods() {
-		//这个操作被定义用来呈现 管理中心导航菜单
+public function doWebGoods() {
+	global $_W, $_GPC;
+ 
+	$ops = array('display', 'edit', 'delete'); // 只支持此 3 种操作.
+	$op = in_array($_GPC['op'], $ops) ? $_GPC['op'] : 'display';
+ 
+	if($op == 'display'){
+ 
+		// 处理 GET 提交
+		$pageindex = max(intval($_GPC['page']), 1); // 当前页码
+		$pagesize = 2; // 设置分页大小
+ 
+		$where = ' WHERE uniacid=:uniacid';
+		$params = array(
+			':uniacid'=>$_W['uniacid']
+		);
+		if (!empty($_GPC['keyword'])) {
+			$where .= ' AND ( (`name` like :keyword) OR (`sn` like :keyword) )';
+			$params[':keyword'] = "%{$_GPC['keyword']}%";
+		}
+		if (!empty($_GPC['status'])) {
+			$where .= ' AND (status = :status)';
+			$params[':status'] = intval($_GPC['status']);
+		}
+		if (!empty($_GPC['categoryid'])) {
+			$where .= ' AND (categoryid = :categoryid)';
+			$params[':categoryid'] = intval($_GPC['categoryid']);
+		}
+ 
+		$sql = 'SELECT COUNT(*) FROM '.tablename($this->tb_goods).$where;
+		$total = pdo_fetchcolumn($sql, $params);
+		$pager = pagination($total, $pageindex, $pagesize);
+ 
+		$sql = 'SELECT * FROM '.tablename($this->tb_goods)." {$where} ORDER BY id asc LIMIT ".(($pageindex -1) * $pagesize).','. $pagesize;
+		$goodses = pdo_fetchall($sql, $params, 'id');
+ 
+		$categories = $this->getAllCategory();
+ 
+		load()->func('tpl');
+		include $this->template('goods_display');
 	}
+ 
+	if ($op == 'edit') {
+ 
+		$id = intval($_GPC['id']);
+		if(!empty($id)){
+			$sql = 'SELECT * FROM '.tablename($this->tb_goods).' WHERE id=:id AND uniacid=:uniacid LIMIT 1';
+			$params = array(':id'=>$id, ':uniacid'=>$_W['uniacid']);
+			$goods = pdo_fetch($sql, $params);
+ 
+			if(empty($goods)){
+				message('未找到指定的商品.', $this->createWebUrl('goods'));
+			}
+		}
+ 
+		$categories = $this->getAllCategory();
+ 
+		if (checksubmit()) {
+			$data = $_GPC['goods']; // 获取打包值
+ 
+			empty($data['name']) && message('请填写商品名称');
+			empty($data['sn']) && message('请填写商品编码');
+			empty($data['image']) && message('请上传商品外观图片');
+			empty($data['categoryid']) && message('请选择商品分类');
+			empty($data['cost']) && message('请填写商品进价');
+			empty($data['price']) && message('请填写商品售价');
+			empty($data['quantity']) && message('请填写商品库存');
+ 
+			if(empty($goods)){
+				$data['uniacid'] = $_W['uniacid'];
+				$data['createtime'] = TIMESTAMP;
+ 
+				$ret = pdo_insert($this->tb_goods, $data);
+				if (!empty($ret)) {
+					$id = pdo_insertid();
+				}
+			} else {
+				$ret = pdo_update($this->tb_goods, $data, array('id'=>$id));
+			}
+ 
+			if (!empty($ret)) {
+				message('商品信息保存成功', $this->createWebUrl('goods', array('op'=>'edit', 'id'=>$id)), 'success');
+			} else {
+				message('商品信息保存失败');
+			}
+		}
+ 
+		load()->func('tpl');
+		include $this->template('goods_edit');
+	}
+ 
+	if($op == 'delete') {
+		$id = intval($_GPC['id']);
+		if(empty($id)){
+			message('未找到指定商品分类');
+		}
+		$result = pdo_delete($this->tb_goods, array('id'=>$id, 'uniacid'=>$_W['uniacid']));
+		if(intval($result) == 1){
+			message('删除商品成功.', $this->createWebUrl('goods'), 'success');
+		} else {
+			message('删除商品失败.');
+		}
+	}
+}
 	public function doMobileOrders() {
 		//这个操作被定义用来呈现 微站个人中心导航
 		
