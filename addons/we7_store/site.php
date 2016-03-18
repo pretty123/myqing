7f7407b165cf5ed9b00158008203c8e4a46b6d36
pretty123 <820168613@qq.com>
2016-03-18 15:38:14 +0800

    s

--- addons/we7_store/site.php
+++ addons/we7_store/site.php
@@ -146,7 +146,7 @@ public function doWebOrders() {
 
 	$ops = array('display');
 	$op = in_array($_GPC['op'], $ops) ? $_GPC['op'] : 'display';
-
+return $op;exit;
 	if($op == 'display'){
 		// 处理 GET 提交
 		$pageindex = max(intval($_GPC['page']), 1); // 当前页码
@@ -196,7 +196,7 @@ public function doWebOrders() {
 		}
 		unset($key);
 		unset($order);
-return 'aa';exit;
+
 		load()->func('tpl');
 		include $this->template('orders_display');
 	}
